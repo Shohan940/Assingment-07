db186f4c64323690791a27ac43ecc7d8a4670c98
Shohan940 <shohansorkar0924@gmail.com>
2026-04-16 07:55:23 +0600

    Add Friendship Analytics

--- src/component/stats/Stats.jsx
+++ src/component/stats/Stats.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Legend, Pie, PieChart, Tooltip } from 'recharts';
+import { InteractionContext } from '../../Context/InteractionContext';
 
 const Stats = () => {
+    const { interactions } = useContext(InteractionContext);
+    const callCount = interactions.filter(call=>call.type=='call').length;
+    const textCount = interactions.filter(text=>text.type=='text').length;
+    const videoCount = interactions.filter(video=>video.type=='video').length;
+    // console.log(callCount);
+
+    const data = [
+        { name: 'call', value:callCount, fill: '#AC7F5E' },
+        { name: 'text', value: textCount, fill: '#7E35E1' },
+        { name: 'video', value: videoCount, fill: '#244D3F' },
+
+    ];
     return (
-        <div>
-            <h1>stats page</h1>
+        <div className='max-w-300 mx-auto'>
+            <h1 className='text-2xl font-bold text-[#244D3F]'>Friendship Analytics</h1>
+            <div className='flex justify-center shadow-xl m-6 p-6'>
+                <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }} responsive>
+                    <Pie
+                        data={data}
+                        innerRadius="80%"
+                        outerRadius="100%"
+                        // Corner radius is the rounded edge of each pie slice
+                        cornerRadius="50%"
+                        fill="#8884d8"
+                        // padding angle is the gap between each pie slice
+                        paddingAngle={5}
+                        dataKey="value"
+                        isAnimationActive={true}
+                    />
+                    <Legend />
+                    <Tooltip />
+                </PieChart>
+            </div>
         </div>
     );
 };
